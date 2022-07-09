6430980d75a05c2935106ac9d403161ad5d50776
Anto Akoke Victor <veeqanto@gmail.com>
2022-07-09 13:14:56 +0100

    no items on cart notification

--- resources/views/pages/frontend/cart/index.blade.php
+++ resources/views/pages/frontend/cart/index.blade.php
@@ -29,58 +29,70 @@ My Cart
 <!-- Cart Area Strat-->
 <section class="cart-area pt-100 pb-100">
     <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <form action="#">
-                <div class="table-content table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th class="cart-product-name">Product</th>
-                                <th class="product-price">Unit Price</th>
-                                <th class="product-quantity">Quantity</th>
-                                <th class="product-subtotal">Total</th>
-                                <th class="product-remove">Remove</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                          @if(count($items))
-                          @foreach($items as $index=>$item)
-                            <tr>
-                                <td class="product-name"><a href="product-details.html">{{ $item->title }}</a></td>
-                                <td class="product-price"><span class="amount">{{ '₦ '.number_format($item->price) }}</span></td>
-                                <td class="product-quantity">
-                                    <div class="cart-plus-minus"><input type="text" value="1" min="1"/></div>
-                                </td>
-                                <td class="product-subtotal"><span class="amount">{{ '₦ '. number_format($item->quantity * $item->price) }}</span></td>
-                                <td class="product-remove"><a href="{{ route('cart.remove', $index) }}"><i class="fa fa-times"></i></a></td>
-                            </tr>
-                            @endforeach
-                            @else
-                          @endif
-                        </tbody>
-                    </table>
-                </div>
+      @if(count($items))
+      <div class="row">
+          <div class="col-12">
+              <form action="#">
+                  <div class="table-content table-responsive">
+                      <table class="table">
+                          <thead>
+                              <tr>
+                                  <th class="cart-product-name">Product</th>
+                                  <th class="product-price">Unit Price</th>
+                                  <th class="product-quantity">Quantity</th>
+                                  <th class="product-subtotal">Total</th>
+                                  <th class="product-remove">Remove</th>
+                              </tr>
+                          </thead>
+                          <tbody>
 
-                @php $total = 0 @endphp
-                  @foreach($items as $id => $details)
-                      @php $total += $details->price * $details->quantity @endphp
-                  @endforeach
-                <div class="row justify-content-end">
-                    <div class="col-md-5 ml-auto">
-                        <div class="cart-page-total">
-                            <h2>Cart totals</h2>
-                            <ul class="mb-20">
-                                <li>Subtotal <span>{{ '₦'.number_format($total) }}</span></li>
-                                <li>Total <span>{{ '₦'.number_format($total) }}</span></li>
-                            </ul>
-                            <a class="s-btn s-btn-2" href="checkout.html">Proceed to checkout</a>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
+                            @foreach($items as $index=>$item)
+                              <tr>
+                                  <td class="product-name"><a href="product-details.html">{{ $item->title }}</a></td>
+                                  <td class="product-price"><span class="amount">{{ '₦ '.number_format($item->price) }}</span></td>
+                                  <td class="product-quantity">
+                                      <div class="cart-plus-minus"><input type="text" value="1" min="1"/></div>
+                                  </td>
+                                  <td class="product-subtotal"><span class="amount">{{ '₦ '. number_format($item->quantity * $item->price) }}</span></td>
+                                  <td class="product-remove"><a href="{{ route('cart.remove', $index) }}"><i class="fa fa-times"></i></a></td>
+                              </tr>
+                              @endforeach
+
+                          </tbody>
+                      </table>
+                  </div>
+
+                  @php $total = 0 @endphp
+                    @foreach($items as $id => $details)
+                        @php $total += $details->price * $details->quantity @endphp
+                    @endforeach
+                  <div class="row justify-content-end">
+                      <div class="col-md-5 ml-auto">
+                          <div class="cart-page-total">
+                              <h2>Cart totals</h2>
+                              <ul class="mb-20">
+                                  <li>Subtotal <span>{{ '₦'.number_format($total) }}</span></li>
+                                  <li>Total <span>{{ '₦'.number_format($total) }}</span></li>
+                              </ul>
+                              <a class="s-btn s-btn-2" href="checkout.html">Proceed to checkout</a>
+                          </div>
+                      </div>
+                  </div>
+              </form>
+          </div>
+      </div>
+      @else
+
+      <div class="row justify-content-center">
+    		<div class="col-lg-8">
+    			<div class="card border-width-3 border-radius-0 border-color-success">
+    				<div class="card-body text-center">
+    					<p class="text-color-dark font-weight-bold text-4-5 mb-0"><i class="fas fa-times text-color-danger mr-1"></i> You have no items(s) in your cart!</p>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+      @endif
     </div>
 </section>
 <!-- Cart Area End-->
